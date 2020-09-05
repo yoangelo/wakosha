934211e86d92d24d3af680f6352169ead87a785c
seiya_t <takeno_s@beartail.jp>
2020-09-06 00:39:29 +0900

    [add]Productsコントローラ各アクション追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,31 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
 
   def show
+    @products = Product.find(params[:id])
   end
 
   def new
+    @product = Product.new
+  end
+
+  def create
+    product = Product.new(product_params)
+    product.save!
+    redirect_to products_path, notice: "#{product.name}を登録しました。"
   end
 
   def edit
   end
 
   def destroy
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :descriprion)
   end
 end
