464b05e755b109efcd2db9d07142b1fba4d83dfb
Seiya Takeno <56621211+yoangelo@users.noreply.github.com>
2020-09-07 06:07:10 +0900

    シンプルなCRUD機能の作成 (#3)
    * [add]productモデル追加
    
    * [add]productsのリソース追加
    
    * [delete]ミニテストは使用しないため削除
    
    * [add]productsコントローラー追加
    
    * [update].ruby-versionをgitの追跡から外す
    
    * [fix].ruby-versionを外す
    
    * [add]Bootstrapの追加
    
    * [add]事業登録(仮)
    
    * [add]Productsコントローラ各アクション追加
    
    * [add]alertクラスの追加
    
    * [add]productsモデルの翻訳情報の追加
    
    * [add]no_image画像の追加
    
    * [fix]@product修正
    
    * [add]products一覧表示(仮)
    
    * [add]product詳細表示[仮]
    
    * [fix]descrip"r"ion → descrip"t"ion
    
    * [add]edit,destroyアクションの追加
    
    * [add]edit画面の追加
    
    * [add]編集,削除ボタンの追加
    
    * [fix]投稿フォームのパーシャル化
    
    * [add]投稿フォームのパーシャル化

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,41 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    product = Product.new(product_params)
+    product.save!
+    redirect_to products_path, notice: "#{product.name}を登録しました。"
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    product = Product.find(params[:id])
+    product.update!(product_params)
+    redirect_to products_path, notice: "#{product.name}を編集しました。"
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    redirect_to products_path, notice: "#{product.name}を削除しました。"
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description)
+  end
+end
