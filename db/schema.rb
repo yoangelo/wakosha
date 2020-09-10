c025e08b9799c17802361df9961d199f3d80ebc0
seiya_t <takeno_s@beartail.jp>
2020-09-10 16:33:55 +0900

    [update]Userモデルにadmin権限を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_10_070459) do
+ActiveRecord::Schema.define(version: 2020_09_10_072916) do
 
   create_table "products", force: :cascade do |t|
     t.string "name", limit: 31, null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_09_10_070459) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
