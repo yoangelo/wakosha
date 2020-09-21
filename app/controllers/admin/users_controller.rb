ef21df3dc2bf476693a9e6c5b200a654cc4f3d02
seiya_t <takeno_s@beartail.jp>
2020-09-21 21:34:31 +0900

    [fix]タイポ

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -19,14 +19,14 @@ class Admin::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to admin_users_url, notice: "「#{@user.name}」を登録しました。"
+      redirect_to admin_users_url(@user), notice: "「#{@user.name}」を登録しました。"
     else
       render :new
     end
   end
 
   def update
-    @user = User.find(:params[:id])
+    @user = User.find(params[:id])
 
     if @user.update(user_params)
       redirect_to admin_user_url(@user), notice: "「#{@user.name}」を更新しました。"
