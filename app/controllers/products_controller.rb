1a851c77097291e74ee3986106e64e4adddad183
seiya_t <takeno_s@beartail.jp>
2020-09-24 04:55:17 +0900

    [update]recentスコープ(昇順5件表示)の追加(とりあえず)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   before_action :login_required, only: [ :new, :create, :edit, :update, :destroy]
 
   def index
-    @products = Product.all
+    @products = Product.all.recent
   end
 
   def show
