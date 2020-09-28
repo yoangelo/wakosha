fab3573678daf7d0ffd757357b5fabb1920e5624
seiya_t <takeno_s@beartail.jp>
2020-09-28 22:36:43 +0900

    [update]imageのstrongparameters追加#7

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -41,6 +41,6 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :description)
+    params.require(:product).permit(:name, :description, images: [])
   end
 end
